114e8ee75631dcb3b7caf1c854259a2455875154
josuesf <113552@unsaac.edu.pe>
2018-04-11 17:28:52 -0500

    mi ultimo commit

--- src/header/index.js
+++ src/header/index.js
@@ -3,7 +3,7 @@ var empty = require('empty-element');
 
 var el = yo`<div class="pos-f-t">
     <nav class="navbar navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" id="sidebarCollapse" type="button">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="float-left">
