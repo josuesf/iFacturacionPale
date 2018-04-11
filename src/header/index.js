b14bb7aff7d2e2bd064f4dccc17fad65a961e172
josuesf <113552@unsaac.edu.pe>
2018-04-11 17:08:46 -0500

    sidebar

--- src/header/index.js
+++ src/header/index.js
@@ -1,40 +1,27 @@
 var yo = require('yo-yo');
 var empty = require('empty-element');
 
-var el = yo`<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="#">Navbar</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
+var el = yo`<nav class="navbar navbar-expand-sm navbar-light bg-faded">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
 </button>
 
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Something else here</a>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li>
-  </ul>
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</div>
+<!-- Brand -->
+<a class="navbar-brand" href="#">Logo</a>
+
+<!-- Links -->
+<div class="collapse navbar-collapse justify-content-end" id="nav-content">   
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="#">Link 1</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Link 2</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Link 3</a>
+</li>
+</ul>
 </nav>`;
 
 module.exports = function header (ctx, next) {
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,9 +2,10 @@ var page = require('page');
 var empty = require('empty-element');
 var title = require('title');
 var header = require('../header');
+var navegador = require('../navegador')
 
-page('/', header, function (ctx, next) {
-  title('Platzigram');
+page('/',navegador,header, function (ctx, next) {
+  title('iFacturacion');
   //var main = document.getElementById('main-container');
 
   //empty(main).appendChild(template(ctx.pictures));
