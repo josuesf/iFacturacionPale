4e384447e5beb390c03e75627d9fa058b25f7823
franciscosf <francisco.farfan.lazo@gmail.com>
2018-04-11 17:09:02 -0500

    Navbar

--- src/header/index.js
+++ src/header/index.js
@@ -1,41 +1,24 @@
 var yo = require('yo-yo');
 var empty = require('empty-element');
 
-var el = yo`<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="#">Navbar</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Something else here</a>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li>
-  </ul>
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+var el = yo`<div class="pos-f-t">
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="float-left">
+            <div class="row justify-content-end text-white">
+                <div class="col">
+                    Nombre de Usuario
+                </div>
+                <div class="col">
+                    <img src="https://scontent.flim5-3.fna.fbcdn.net/v/t1.0-9/22688495_1570753256315987_747197238207117949_n.jpg?_nc_cat=0&oh=9741eb12275aa78b1775ab729dff9660&oe=5B6684AB" width="50" height="50" alt="..." class="rounded-circle">
+                </div>
+            </div>
+        </div>
+    </nav>
 </div>
-</nav>`;
+`;
 
 module.exports = function header (ctx, next) {
   var container = document.getElementById('header-container')
