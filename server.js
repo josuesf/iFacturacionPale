d585942e4b53e48689eebcf1a87c58bbeee7bc99
josuesf <113552@unsaac.edu.pe>
2018-04-11 10:19:04 -0500

    configuracion de servidor

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('picture');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'iFacturacion' });
+})
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Escuchando en el puerto 3000');
+})
