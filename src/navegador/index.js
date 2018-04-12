5766921f5fff1d5a55cd91c808881a719519c02c
franciscosf <francisco.farfan.lazo@gmail.com>
2018-04-12 09:54:02 -0500

    Merge

--- src/navegador/index.js
+++ src/navegador/index.js
@@ -2,24 +2,26 @@ var yo  =  require('yo-yo')
 var empty = require('empty-element');
 
 var el = yo`<div id="sidebar"><div class="sidebar-header">
-<h3>Collapsible Sidebar</div>
+<h5>Palerp</h5>
 </div>
 
 <!-- Sidebar Links -->
 <ul class="list-unstyled components">
-<li class="active"><a href="#">Home</a></li>
-<li><a href="#">About</a></li>
+<li class="active"><a href="#">Inicio</a></li>
+<li><a href="#">Comercial</a></li>
 
 <li><!-- Link with dropdown items -->
-    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Pages</a>
+    <a href="#homeSubmenu" class="nav-link dropdown-toggle" data-toggle="collapse" aria-expanded="false">Contabilidad</a>
     <ul class="collapse list-unstyled" id="homeSubmenu">
         <li><a href="#">Page</a></li>
         <li><a href="#">Page</a></li>
         <li><a href="#">Page</a></li>
     </ul>
 
-<li><a href="#">Portfolio</a></li>
-<li><a href="#">Contact</a></li>
+<li><a href="#">Logistica</a></li>
+<li><a href="#">Activos Fijos</a></li>
+<li><a href="#">Planillas</a></li>
+<li><a href="#">Configuracion</a></li>
 </ul></div>`;
 
 module.exports = function navegador (ctx, next) {
