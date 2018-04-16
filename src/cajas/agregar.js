fa3fadad52add86ae4c5ee43d28ef95b8eda8dd1
franciscosf <francisco.farfan.lazo@gmail.com>
2018-04-16 17:47:54 -0500

    Changes

--- src/cajas/agregar.js
+++ src/cajas/agregar.js
@@ -1,9 +1,8 @@
 var empty = require('empty-element');
 var yo = require('yo-yo');
+import {ListarCajas} from './listar'
 
-
-function NuevaCaja(_escritura,sucursales,caja) {
-    console.log(caja)
+function NuevaCaja(_escritura,sucursales,usuarios,cuentas_contables,caja) {
 
     var el = yo`
     <div>
@@ -53,7 +52,7 @@ function NuevaCaja(_escritura,sucursales,caja) {
                                             
                                             <div class="checkbox">
                                                 <label>
-                                                    <input type="checkbox" id="Flag_Activo" value="${caja?caja.Flag_Activo:'0'}"> Es Activo?
+                                                    <input type="checkbox" id="Flag_Activo" value="${caja?caja.Flag_Activo:0}"> Es Activo?
                                                 </label>
                                             </div>
                                         </div>
@@ -63,7 +62,7 @@ function NuevaCaja(_escritura,sucursales,caja) {
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="Des_Caja">Nombre de la Caja</label>
-                                            <input type="text" class="form-control" id="Des_Caja" placeholder="Ingrese Contrasena">
+                                            <input type="text"  style="text-transform:uppercase" class="form-control" id="Des_Caja" placeholder="Ingrese Nombre de caja" value="${caja?caja.Des_Caja:''}">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
@@ -79,16 +78,94 @@ function NuevaCaja(_escritura,sucursales,caja) {
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="Cod_UsuarioCajero">Usuario o vendedor responsable</label>
-                                            <input type="text" style="text-transform:uppercase" class="form-control" id="Cod_UsuarioCajero" placeholder="Usuario responsable" value="${caja?caja.Cod_UsuarioCajero:''}">
+                                            <select id="Cod_UsuarioCajero" class="form-control select2">
+                                                ${sucursales.map(e=>yo`<option style="text-transform:uppercase" value="${e}" ${caja?caja.Cod_Sucursal == e?'selected':'':''}>${e}</option>`)}
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="Cod_CuentaContable">Cuenta Contable</label>
-                                            <input type="text" style="text-transform:uppercase" class="form-control" id="Cod_CuentaContable" placeholder="Cuenta Contable" value="${caja?caja.Cod_CuentaContable:''}">
+                                            <select id="Cod_CuentaContable" class="form-control select2">
+                                                ${sucursales.map(e=>yo`<option style="text-transform:uppercase" value="${e.Cod_CuentaContable}" ${caja?caja.Cod_Sucursal == e?'selected':'':''}>${e}</option>`)}
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
+                                ${caja!=undefined? yo `
+                                    <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="nav-tabs-custom">
+                                                    <ul class="nav nav-tabs">
+                                                        <li class="active">
+                                                            <a href="#tab_1" data-toggle="tab" aria-expanded="true">
+                                                                <i class="fa fa-file"></i> Documentos Relacionados</a>
+                                                        </li>
+                                                        <li class="">
+                                                            <a href="#tab_2" data-toggle="tab" aria-expanded="false">
+                                                                <i class="fa fa-star"></i> Productos Favoritos</a>
+                                                        </li>
+                                                        <li class="dropdown">
+                                                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                                                                Acciones
+                                                                <span class="caret"></span>
+                                                            </a>
+                                                            <ul class="dropdown-menu">
+                                                                <li role="presentation">
+                                                                    <a role="menuitem" tabindex="-1" href="#">Nuevo Documento</a>
+                                                                </li>
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
+                                                    <div class="tab-content">
+                                                        <div class="tab-pane active" id="tab_1">
+                                                            <div class="table-responsive">
+                                                                <table class="table table-bordered table-striped">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Item</th>
+                                                                            <th>Documento</th>
+                                                                            <th>Serie</th>
+                                                                            <th>Impresora</th>
+                                                                            <th>Imprimir</th>
+                                                                            <th>Nombre Archivo</th>
+                                                                            <th>Rapida</th>
+                                                                            <th>Ticketera</th>
+                                                                            <th>Acciones</th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                        <!-- /.tab-pane -->
+                                                        <div class="tab-pane" id="tab_2">
+                                                            <div class="table-responsive">
+                                                                <table class="table table-bordered table-striped">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Productos</th>
+                                                                            <th>Almacen</th>
+                                                                            <th>Unidad Medida</th>
+                                                                            <th>Precio</th>
+                                                                            <th>Stock</th>
+                                                                            <th>Acciones</th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /.tab-content -->
+                                                </div>
+                                            </div>
+                                        
+                                        </div>    
+                                    `:yo``}
+                                
                             </div>
                             <!-- /.box-body -->
                 
@@ -104,6 +181,7 @@ function NuevaCaja(_escritura,sucursales,caja) {
     </div>`
     var main = document.getElementById('main-contenido');
     empty(main).appendChild(el);
+    // $('.select2').select2();
 }
 
 function Guardar(_escritura, caja){
