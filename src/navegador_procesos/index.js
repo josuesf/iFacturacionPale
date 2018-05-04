974d3323755f1ef5ce894ecf327da3b0d4312f68
omaralex <omar.muniz48@gmail.com>
2018-05-04 17:22:55 -0500

    merge navegador procesos

--- src/navegador_procesos/index.js
+++ src/navegador_procesos/index.js
@@ -1,14 +1,15 @@
 var yo = require('yo-yo')
 var empty = require('empty-element');
 
-//Views Egresos
+//Views CompraVenta
 import { NuevoCompraVentaDolar } from '../sis_procesos/mod_ventas/mod_compra_venta_moneda_extranjera/agregar'
 
+import { NuevoIngreso } from '../sis_procesos/mod_ventas/recibo_ingreso'
 var el = yo`
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">Ventas</li>
     <li class="treeview">
-        <a href="#"W>
+        <a href="#">
             <i class="fa fa-home"></i> <span>Movimientos de caja</span>
             <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
@@ -17,6 +18,8 @@ var el = yo`
         <ul class="treeview-menu">
             <li class=""><a href="#" onclick=${() => console.log('hola')}><i class="fa fa-circle-o"></i> Recibo de Ingresos</a></li>
             <li class=""><a href="#" onclick=${() => NuevoCompraVentaDolar(true)}><i class="fa fa-circle-o"></i> Compra y Venta de Dolar</a></li>
+            <li class=""><a  onclick=${() => NuevoIngreso()}><i class="fa fa-circle-o"></i> Recibo de Ingresos</a></li>
+            <li class=""><a href="#" onclick=${() => console.log('hola')}><i class="fa fa-circle-o"></i> Recibo de Egresos</a></li>
         </ul>
     </li>
     
@@ -25,5 +28,5 @@ var el = yo`
 module.exports = function navegador(ctx, next) {
     var container = document.getElementById('nav-container')
     empty(container).appendChild(el);
-    // next();
+    
 }
